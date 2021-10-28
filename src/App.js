906736156f5d68c8bfb07bd0d17271a512236dee
Anabiatahir3 <anabiatahir365@gmail.com>
2021-10-29 00:26:50 +0500

    Github-finder
    implemented context API and fixed warnings

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
-import './App.css';
-import React, { Component } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
-import axios from 'axios';
-class App extends Component {
-   async componentDidMount(){
-    const res=await axios.get("https://api.github.com/users")
-    console.log(res.data)  ;
-    }
-  render() {
-    return (
-      <div className='App'>
-        <Navbar />
-        <div className="container"><Users/></div>
-      </div>
-    );
-  }
+import Home from './components/pages/Home';
+import User from './components/users/User';
+import Alert from './components/layout/Alert';
+import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
+import GithubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
+import './App.css';
+
+const App = () => {
+  return (
+    <GithubState>
+      <AlertState>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <div className="container">
+              <Alert />
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/about' component={About} />
+                <Route exact path='/user/:login' component={User} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </AlertState>
+    </GithubState>
+  );  
 }
 
 export default App;
